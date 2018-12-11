6a55146a0215f86b13383dca923ce737d31083e4
Brandon Monier <brandon.monier@gmail.com>
2018-12-11 18:08:12 -0500

    Add package startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,14 @@
 #' @importFrom rJava .jpackage
 #' @export
 .onLoad <- function(libname, pkgname) {
-    rJava::.jpackage(pkgname, lib.loc = libname)
-}
-
-
-#' @export
-.onAttach <- function(libname, pkgname) {
     packageStartupMessage(
         paste0(
-            "Welcome to rTASSEL\n",
-            "                 --- NOTE ---\n",
+            "Welcome to rTASSEL\n\n",
+            "--- NOTE ---\n",
             "Keep in mind that this package is in the early\n",
             "stages of development and some functionalities\n",
             "may not work..."
         )
     )
+    rJava::.jpackage(pkgname, lib.loc = libname)
 }
